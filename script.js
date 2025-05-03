01db913134aa00e4816a49d06efd07eeb225c281
kackur <44867189+kackur@users.noreply.github.com>
2025-05-03 20:20:27 +0200

    Update script.js
    fetch("https://ovningsgenerator-8h3djxg8q-kackurs-projects.vercel.app/api/generate", {

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function draw(data) {
 async function generate() {
   const description = document.getElementById("description").value;
 
-  const response = await fetch("https://ovningsgenerator.vercel.app/api/generate", {
+  const response = await fetch("https://ovningsgenerator-8h3djxg8q-kackurs-projects.vercel.app/api/generate", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ description })
