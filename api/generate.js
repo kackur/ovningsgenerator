6dbab644a4cc92ede560df9806a61b571d652571
kackur <44867189+kackur@users.noreply.github.com>
2025-05-04 08:30:17 +0200

    Update generate.js
    console log

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,5 @@
+console.log("🚀 Startar /api/generate");
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
